0f553f7d0c3bf52ed5853864c0b6a6c3113ee4ab
Jursdotme <rasmus@jurs.me>
2014-03-25 21:32:17 +0100

    Started adding UI kit

---
+++ template-uikit-demo.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: UI Kit
+*/
+?>
+
+<?php  get_header();?>
+
+<div class="content-1">
+  <div>
+    <div class="container">
+      <div class="hero-unit">
+        <h1>Get ready for a whole new level<br>of learning to cook!</h1>
+        <p class="lead">Very soon we will launch and you will be able to see it yourself.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include "edit_post_link.php" ?>
+<?php  get_footer();?>
