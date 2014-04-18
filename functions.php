b3333463c84f8d6ea94b870ece8253415f00fab5
Rasmus Jürs <rasmus@jurs.me>
2014-04-18 13:39:56 +0200

    Add Image_sizes to separate file

--- functions.php
+++ functions.php
@@ -15,6 +15,7 @@ require_once( 'partials/functions/widgets.php' ); // this comes turned off by de
 require_once( 'partials/functions/nav_menus.php' ); // this comes turned off by default
 require_once( 'partials/functions/comments-callback.php' ); // this comes turned off by default
 require_once( 'partials/functions/excerpt.php' ); // this comes turned off by default
+require_once( 'partials/functions/image_sizes.php' ); // this comes turned off by default
 
 /*------------------------------------*\
 	Theme Support
@@ -29,13 +30,6 @@ if (function_exists('add_theme_support'))
 {
     // Add Menu Support
     add_theme_support('menus');
-
-    // Add Thumbnail Theme Support
-    add_theme_support('post-thumbnails');
-    // add_image_size('large', 700, '', true); // Large Thumbnail
-    // add_image_size('medium', 250, '', true); // Medium Thumbnail
-    // add_image_size('small', 120, '', true); // Small Thumbnail
-    // add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
 
     // Enables post and comment RSS feed links to head
     add_theme_support('automatic-feed-links');
