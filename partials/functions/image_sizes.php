58ce871171e28759b97ea435b4b08ef4c859255f
Rasmus Jürs <rasmus@jurs.me>
2014-04-21 10:31:36 +0200

    Add more generous thumbnail size

--- partials/functions/image_sizes.php
+++ partials/functions/image_sizes.php
@@ -4,6 +4,7 @@
 
 // Add Thumbnail Theme Support
 add_theme_support('post-thumbnails');
+ add_image_size('thumbnail', 300, 300, true); // Large Thumbnail
 // add_image_size('large', 700, '', true); // Large Thumbnail
 // add_image_size('medium', 250, '', true); // Medium Thumbnail
 // add_image_size('small', 120, '', true); // Small Thumbnail
