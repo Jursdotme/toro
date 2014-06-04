f73896e2bae2f80a58b6040872c48fae461bb8d3
Rasmus Jürs <rasmus@jurs.me>
2014-06-04 15:16:38 +0200

    Start on Page Builder

---
+++ page_templates/page-builder.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: Page Builder
+*/
+?>
+
+<?php get_header(); ?>
+
+<?php get_template_part('page_templates/page_builder/layout', '1'); ?>
+<?php get_template_part('page_templates/page_builder/layout', '2'); ?>
+<?php get_template_part('page_templates/page_builder/layout', '3'); ?>
+
+<?php get_footer(); ?>
---
+++ page_templates/page_builder/layout-2.php
@@ -0,0 +1,38 @@
+<!-- Layout 2 -->
+<div class="layout-2">
+  <div class="container">
+    <div class="col-sm-12">
+      <img src="http://www.placehold.it/300x300/d03530/fff&text=Image" class="img-circle">
+      <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+      <h4>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</h4>
+      <div class="row">
+
+        <div class="col-sm-5 col-sm-offset-1">
+          <h3>Nullam id dolor id nibh ultricies ut.</h3>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Vestibulum id ligula porta felis euismod semper.</p>
+        </div>
+
+        <div class="col-sm-5 col-sm-offset-1">
+          <h3>Nullam id dolor id nibh ultricies ut.</h3>
+          <p>Nulla vitae elit libero, a pharetra augue. Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper. Vestibulum id ligula porta felis euismod semper.</p>
+        </div>
+
+
+      </div>
+      <div class="row">
+
+        <div class="col-sm-5 col-sm-offset-1">
+          <h3>Nullam id dolor id nibh ultricies ut.</h3>
+          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean lacinia bibendum nulla sed consectetur. Nullam quis risus eget urna mollis ornare vel eu leo.</p>
+        </div>
+
+        <div class="col-sm-5 col-sm-offset-1">
+          <h3>Nullam id dolor id nibh ultricies ut.</h3>
+          <p>Sed posuere consectetur est at lobortis. Maecenas faucibus mollis interdum. Maecenas sed diam eget risus varius blandit sit amet non magna. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Fusce dapibus.</p>
+        </div>
+
+
+      </div>
+    </div>
+  </div>
+</div>
